d942fa35aa68dd00aa85a4a3c2beb58e8a8a7ca7
chpy <a910823>
2018-01-30 23:57:24 +0800

    httphelper get

--- spider/src/main/java/util/httpHelper.java
+++ spider/src/main/java/util/httpHelper.java
@@ -10,163 +10,180 @@ import java.util.stream.Collectors;
 
 public class httpHelper {
 
-	private static final String DEFAULT_CODING = "utf-8";
+    private static final String DEFAULT_CODING = "utf-8";
 
-	private static final HashMap<String, String> HEADER_CONF = new HashMap<String, String>() {
-		{
-			put("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36HH=17 DOL/HELLO_GWF_");
-			//put("", "");
-		}
-	};
+    private static final HashMap<String, String> HEADER_CONF = new HashMap<String, String>() {
+        {
+            put("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36HH=17 DOL/HELLO_GWF_");
+            //put("", "");
+        }
+    };
 
-	public static String get(String url, Map<String, String> params) {
-		return get(url, params, DEFAULT_CODING);
-	}
+    public static String get(String url, Map<String, String> params) {
+        return get(url, params, DEFAULT_CODING, null);
+    }
 
-	public static String get(String url, Map<String, String> params, String encoding) {
-		String responseRes = "";
-		HttpURLConnection con = null;
-		BufferedReader br = null;
-		try {
-			String paramStr = String.join("&", params.entrySet().stream().map(
-					t -> t.getKey() + "=" + getUrlEncoding(t.getValue())).collect(Collectors.toList()));
-			URL uri = new URL(url + "?" + paramStr);
-			con = (HttpURLConnection) uri.openConnection();
+    public static String get(String url, Map<String, String> params, Map<String, String> headers) {
+        return get(url, params, DEFAULT_CODING, headers);
+    }
 
-			con.setRequestMethod("GET");
-			setHttpHeads(con, HEADER_CONF);
-			con.setConnectTimeout(60000);
-			con.setDoInput(true);
-			con.setDoOutput(true);
-			con.connect();
+    public static String get(String url, Map<String, String> params, String encoding, Map<String, String> headers) {
+        String responseRes = "";
+        HttpURLConnection con = null;
+        BufferedReader br = null;
+        try {
+            String paramStr = String.join("&", params.entrySet().stream().map(
+                    t -> t.getKey() + "=" + getUrlEncoding(t.getValue())).collect(Collectors.toList()));
+            URL uri = new URL(url + "?" + paramStr);
+            con = (HttpURLConnection) uri.openConnection();
 
-			int code = con.getResponseCode();
-			if (code == 200) {
-				StringBuffer resultBuffer = new StringBuffer();
-				br = new BufferedReader(new InputStreamReader(con.getInputStream(), encoding));
-				String temp;
-				while ((temp = br.readLine()) != null) {
-					resultBuffer.append(temp);
-				}
-				responseRes = resultBuffer.toString();
-			}
-		} catch (Exception ex) {
-			LogUtil.error("get request failed", ex);
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-					throw new RuntimeException(e);
-				} finally {
-					br = null;
-					if (con != null) {
-						con.disconnect();
-						con = null;
-					}
-				}
-			}
-			if (con != null) {
-				con.disconnect();
-				con = null;
-			}
-		}
-		return responseRes;
-	}
+            con.setRequestMethod("GET");
+            setHttpHeads(con, HEADER_CONF);
+            if (headers != null) {
+                setHttpHeads(con, headers);
+            }
+            con.setConnectTimeout(60000);
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.connect();
 
-	private static String getUrlEncoding(String text) {
-		String endcodingText = "error";
-		try {
-			endcodingText = URLEncoder.encode(text, "utf-8");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			LogUtil.error("getUrlEncoding", e);
-		}
-		return endcodingText;
-	}
+            int code = con.getResponseCode();
+            if (code == 200) {
+                StringBuffer resultBuffer = new StringBuffer();
+                br = new BufferedReader(new InputStreamReader(con.getInputStream(), encoding));
+                String temp;
+                while ((temp = br.readLine()) != null) {
+                    resultBuffer.append(temp);
+                }
+                responseRes = resultBuffer.toString();
+            }
+        } catch (Exception ex) {
+            LogUtil.error("get request failed", ex);
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                } finally {
+                    br = null;
+                    if (con != null) {
+                        con.disconnect();
+                        con = null;
+                    }
+                }
+            }
+            if (con != null) {
+                con.disconnect();
+                con = null;
+            }
+        }
+        return responseRes;
+    }
 
-	public static String post(String url, Map<String, String> params) {
-		String paramStr = String.join("&", params.entrySet().stream().map(
-				t -> t.getKey() + "=" + getUrlEncoding(t.getValue())).collect(Collectors.toList()));
+    public static String post(String url, Map<String, String> params) {
+        String paramStr = String.join("&", params.entrySet().stream().map(
+                t -> t.getKey() + "=" + getUrlEncoding(t.getValue())).collect(Collectors.toList()));
 
-		return post(url, paramStr, "application/x-www-form-urlencoded");
-	}
+        return post(url, paramStr, "application/x-www-form-urlencoded", null);
+    }
 
-	public static String post(String url, String params, String contentType) {
-		String responseRes = "";
-		HttpURLConnection con = null;
-		OutputStreamWriter osw = null;
-		BufferedReader br = null;
-		try {
+    public static String post(String url, Map<String, String> params, Map<String, String> headers) {
+        String paramStr = String.join("&", params.entrySet().stream().map(
+                t -> t.getKey() + "=" + getUrlEncoding(t.getValue())).collect(Collectors.toList()));
 
-			URL uri = new URL(url);
-			con = (HttpURLConnection) uri.openConnection();
+        return post(url, paramStr, "application/x-www-form-urlencoded", headers);
+    }
 
-			con.setRequestMethod("POST");
-			con.setConnectTimeout(60000);
-			con.setDoInput(true);
-			con.setDoOutput(true);
-			con.setUseCaches(false);
-			con.setRequestProperty("Content-Type", contentType);
-			setHttpHeads(con, HEADER_CONF);
-			con.connect();
+    public static String post(String url, String params, String contentType, Map<String, String> headers) {
+        String responseRes = "";
+        HttpURLConnection con = null;
+        OutputStreamWriter osw = null;
+        BufferedReader br = null;
+        try {
 
-			if (params != null && params.length() > 0) {
-				osw = new OutputStreamWriter(con.getOutputStream(), DEFAULT_CODING);
-				osw.write(params);
-				osw.flush();
-			}
-			int code = con.getResponseCode();
-			if (code == 200) {
-				StringBuffer resultBuffer = new StringBuffer();
-				br = new BufferedReader(new InputStreamReader(con.getInputStream(), DEFAULT_CODING));
-				String temp;
-				while ((temp = br.readLine()) != null) {
-					resultBuffer.append(temp);
-				}
-				responseRes = resultBuffer.toString();
-			}
-		} catch (Exception ex) {
-			LogUtil.error("get request failed", ex);
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					br = null;
-					throw new RuntimeException(e);
-				} finally {
-					if (con != null) {
-						con.disconnect();
-						con = null;
-					}
-				}
-				if (osw != null) {
-					try {
-						osw.close();
-					} catch (IOException e) {
-						throw new RuntimeException(e);
-					} finally {
-						osw = null;
-						if (con != null) {
-							con.disconnect();
-							con = null;
-						}
-					}
-				}
-				if (con != null) {
-					con.disconnect();
-					con = null;
-				}
-			}
-		}
-		return responseRes;
-	}
+            URL uri = new URL(url);
+            con = (HttpURLConnection) uri.openConnection();
 
-	private static void setHttpHeads(HttpURLConnection conn, Map<String, String> heads) {
-		for (Map.Entry<String, String> item : heads.entrySet()) {
-			conn.setRequestProperty(item.getKey(), item.getValue());
-		}
-	}
+            con.setRequestMethod("POST");
+            con.setConnectTimeout(60000);
+            setHttpHeads(con, HEADER_CONF);
+            if (headers != null)
+                setHttpHeads(con, headers);
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.setUseCaches(false);
+            con.setRequestProperty("Content-Type", contentType);
+            setHttpHeads(con, HEADER_CONF);
+            con.connect();
+
+            if (params != null && params.length() > 0) {
+                osw = new OutputStreamWriter(con.getOutputStream(), DEFAULT_CODING);
+                osw.write(params);
+                osw.flush();
+            }
+            int code = con.getResponseCode();
+            if (code == 200) {
+                StringBuffer resultBuffer = new StringBuffer();
+                br = new BufferedReader(new InputStreamReader(con.getInputStream(), DEFAULT_CODING));
+                String temp;
+                while ((temp = br.readLine()) != null) {
+                    resultBuffer.append(temp);
+                }
+                responseRes = resultBuffer.toString();
+            }
+        } catch (Exception ex) {
+            LogUtil.error("get request failed", ex);
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    br = null;
+                    throw new RuntimeException(e);
+                } finally {
+                    if (con != null) {
+                        con.disconnect();
+                        con = null;
+                    }
+                }
+                if (osw != null) {
+                    try {
+                        osw.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    } finally {
+                        osw = null;
+                        if (con != null) {
+                            con.disconnect();
+                            con = null;
+                        }
+                    }
+                }
+                if (con != null) {
+                    con.disconnect();
+                    con = null;
+                }
+            }
+        }
+        return responseRes;
+    }
+
+    private static void setHttpHeads(HttpURLConnection conn, Map<String, String> heads) {
+        for (Map.Entry<String, String> item : heads.entrySet()) {
+            conn.setRequestProperty(item.getKey(), item.getValue());
+        }
+    }
+
+    private static String getUrlEncoding(String text) {
+        String endcodingText = "error";
+        try {
+            endcodingText = URLEncoder.encode(text, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            LogUtil.error("getUrlEncoding", e);
+        }
+        return endcodingText;
+    }
 }
