067519fe01167c0123f0b8979fc41cb87b249147
chpy <a910823>
2018-02-11 00:41:56 +0800

    获取营业部购买股票

--- spider/src/main/java/filter/eastmoneyChartsFilter.java
+++ spider/src/main/java/filter/eastmoneyChartsFilter.java
@@ -1,6 +1,5 @@
 package filter;
 
-import entity.DeepCopy;
 import entity.eastmoney.yybIncreaseEntity;
 import entity.eastmoney.yybStockInfo;
 
@@ -13,28 +12,29 @@ import java.util.stream.Collectors;
  */
 public class eastmoneyChartsFilter extends baseFilter<List<yybIncreaseEntity>> {
 
-	/**
-	 * 最近没有股票买卖的剔除
-	 *
-	 * @param data
-	 * @return
-	 */
-	@Override
-	public List<yybIncreaseEntity> filter(List<yybIncreaseEntity> data) {
-		return data.stream().filter(t -> {
-			boolean result = true;
-			//第一天大涨。三天后转跌的剔除
-			if (t.getOneDayIncreaseRate() >= 5 && t.getThreeDayIncreaseRate() <= 0) {
-				result = false;
-			}
-			//过滤购买的股票信息
-			else {
-				List<yybStockInfo> stocks = t.getBuyStock();
-				Map<String, Double> mapInfo = stocks.stream().collect(Collectors.groupingBy(yybStockInfo::getStockCode, Collectors.summingDouble(yybStockInfo::getBuyNum)));
-				//股票卖出额达到买入额一半
+    /**
+     * 最近没有股票买卖的剔除
+     *
+     * @param data
+     * @return
+     */
+    @Override
+    public List<yybIncreaseEntity> filter(List<yybIncreaseEntity> data) {
+        return data.stream().filter(t -> {
+            boolean result = true;
+            //第一天大涨。三天后转跌的剔除
+            if (t.getOneDayIncreaseRate() >= 5 && t.getThreeDayIncreaseRate() <= 0) {
+                result = false;
+            }
+            //过滤购买的股票信息
+            else {
+                List<yybStockInfo> stocks = t.getBuyStock();
+                Map<String, Double> mapInfo = stocks.stream()
+                        .collect(Collectors.groupingBy(yybStockInfo::getStockCode, Collectors.summingDouble(yybStockInfo::getBuyNum)));
+                //股票卖出额达到买入额一半
 
-			}
-			return result;
-		}).collect(Collectors.toList());
-	}
+            }
+            return result;
+        }).collect(Collectors.toList());
+    }
 }
