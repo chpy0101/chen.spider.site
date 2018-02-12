6d5c3b30cc4544d6d8ef6a65bfe16c030e0156b1
chpy <a910823>
2018-02-12 23:19:41 +0800

    营业部购买的股票 分组整理买入卖出信息

--- spider/src/main/java/entity/eastmoney/yybStockSellInfo.java
+++ spider/src/main/java/entity/eastmoney/yybStockSellInfo.java
@@ -1,44 +1,40 @@
 package entity.eastmoney;
 
-public class yybStockSellInfo extends createEntity {
-	public String code;
-	public double sell;
-	public double buy;
+public class yybStockSellInfo {
+    public String code;
+    public double sell;
+    public double buy;
 
-	public String getCode() {
-		return code;
-	}
+    public String getCode() {
+        return code;
+    }
 
-	public void setCode(String code) {
-		this.code = code;
-	}
+    public void setCode(String code) {
+        this.code = code;
+    }
 
-	public double getSell() {
-		return sell;
-	}
+    public double getSell() {
+        return sell;
+    }
 
-	public void setSell(double sell) {
-		this.sell = sell;
-	}
+    public void setSell(double sell) {
+        this.sell = sell;
+    }
 
-	public double getBuy() {
-		return buy;
-	}
+    public double getBuy() {
+        return buy;
+    }
 
-	public void setBuy(double buy) {
-		this.buy = buy;
-	}
+    public void setBuy(double buy) {
+        this.buy = buy;
+    }
 
-	public void addSell(double value) {
-		this.sell += value;
-	}
+    public void addSell(double value) {
+        this.sell += value;
+    }
 
-	public void addBuy(double value) {
-		this.buy += value;
-	}
+    public void addBuy(double value) {
+        this.buy += value;
+    }
 
-	@Override
-	public yybStockSellInfo createInstance() {
-		return new yybStockSellInfo();
-	}
 }
--- spider/src/main/java/util/CollectorsUtil.java
+++ spider/src/main/java/util/CollectorsUtil.java
@@ -1,8 +1,9 @@
 package util;
 
-import entity.eastmoney.createEntity;
-
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
@@ -14,51 +15,50 @@ import java.util.stream.Collector;
  */
 public class CollectorsUtil {
 
-	static class CollectorObjectImpl<T, A, R> implements Collector<T, A, R> {
-		private final Supplier<A> supplier;
-		private final BiConsumer<A, T> accumulator;
-		private final BinaryOperator<A> combiner;
-		private final Function<A, R> finisher;
-		private final Set<Characteristics> characteristics;
+    static class CollectorObjectImpl<T, A, R> implements Collector<T, A, R> {
+        private final Supplier<A> supplier;
+        private final BiConsumer<A, T> accumulator;
+        private final BinaryOperator<A> combiner;
+        private final Function<A, R> finisher;
+        private final Set<Characteristics> characteristics;
 
-		CollectorObjectImpl(Supplier<A> supplier, BiConsumer<A, T> accumulator, BinaryOperator<A> combiner, Function<A, R> finisher, Set<Characteristics> characteristics) {
-			this.supplier = supplier;
-			this.accumulator = accumulator;
-			this.combiner = combiner;
-			this.finisher = finisher;
-			this.characteristics = characteristics;
-		}
+        CollectorObjectImpl(Supplier<A> supplier, BiConsumer<A, T> accumulator, BinaryOperator<A> combiner, Function<A, R> finisher, Set<Characteristics> characteristics) {
+            this.supplier = supplier;
+            this.accumulator = accumulator;
+            this.combiner = combiner;
+            this.finisher = finisher;
+            this.characteristics = characteristics;
+        }
 
-		@Override
-		public BiConsumer<A, T> accumulator() {
-			return accumulator;
-		}
+        @Override
+        public BiConsumer<A, T> accumulator() {
+            return accumulator;
+        }
 
-		@Override
-		public Supplier<A> supplier() {
-			return supplier;
-		}
+        @Override
+        public Supplier<A> supplier() {
+            return supplier;
+        }
 
-		@Override
-		public BinaryOperator<A> combiner() {
-			return combiner;
-		}
+        @Override
+        public BinaryOperator<A> combiner() {
+            return combiner;
+        }
 
-		@Override
-		public Function<A, R> finisher() {
-			return finisher;
-		}
+        @Override
+        public Function<A, R> finisher() {
+            return finisher;
+        }
 
-		@Override
-		public Set<Characteristics> characteristics() {
-			return characteristics;
-		}
-	}
+        @Override
+        public Set<Characteristics> characteristics() {
+            return characteristics;
+        }
+    }
 
-	public static <T, R> Collector<T, Map<String, Object>, R> summingToObject(BiConsumer<T, Map<String, Object>> map) {
-		return new CollectorObjectImpl<T, Map<String, Object>, R>(() -> new HashMap<String, Object>(), (a, b) -> {
-			map.accept(b, a);
-		}, null, a -> , new HashSet<>());
-
-	}
+    public static <T, R> Collector<T, Map<String, String>, R> summingToObject(BiConsumer<T, Map<String, String>> map, Function<Map<String, String>, R> completeMapper) {
+        return new CollectorObjectImpl<>(() -> new HashMap<>(),
+                (a, b) -> map.accept(b, a), null,
+                a -> completeMapper.apply(a), new HashSet<>());
+    }
 }
