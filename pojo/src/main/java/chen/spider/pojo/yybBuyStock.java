9d1fb7e8d378a74b8b565e6ab03d6101a26119d0
chpy <a910823>
2018-03-11 22:54:25 +0800

    hibernate + jpa 调试完成

--- pojo/src/main/java/chen/spider/pojo/yybBuyStock.java
+++ pojo/src/main/java/chen/spider/pojo/yybBuyStock.java
@@ -6,104 +6,102 @@ import java.util.Date;
 @Entity
 @Table(name = "yybbuystock")
 public class yybBuyStock {
+    private Long id;
+    private String buyCode;
+    private String buyName;
+    private String stockCode;
+    private String stockName;
+    private Date buyDate;
+    private Double recommedScore;
+    private Integer buyCount;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
 
-	private Long id;
-	private String buyCode;
-	private String buyName;
-	private String stockCode;
-	private String stockName;
-	private Date buyDate;
-	private Double recommedScore;
-	private Integer buyCount;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.TABLE)
-	public Long getId() {
-		return id;
-	}
+    @Column(name = "buyCode")
+    public String getBuyCode() {
+        return buyCode;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setBuyCode(String buyCode) {
+        this.buyCode = buyCode;
+    }
 
-	@Column(name = "buyCode")
-	public String getBuyCode() {
-		return buyCode;
-	}
+    @Column(name = "buyName")
+    public String getBuyName() {
+        return buyName;
+    }
 
-	public void setBuyCode(String buyCode) {
-		this.buyCode = buyCode;
-	}
+    public void setBuyName(String buyName) {
+        this.buyName = buyName;
+    }
 
-	@Column(name = "buyName")
-	public String getBuyName() {
-		return buyName;
-	}
+    @Column(name = "stockCode")
+    public String getStockCode() {
+        return stockCode;
+    }
 
-	public void setBuyName(String buyName) {
-		this.buyName = buyName;
-	}
+    public void setStockCode(String stockCode) {
+        this.stockCode = stockCode;
+    }
 
-	@Column(name = "stockCode")
-	public String getStockCode() {
-		return stockCode;
-	}
+    @Column(name = "stockName")
+    public String getStockName() {
+        return stockName;
+    }
 
-	public void setStockCode(String stockCode) {
-		this.stockCode = stockCode;
-	}
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
 
-	@Column(name = "stockName")
-	public String getStockName() {
-		return stockName;
-	}
+    @Column(name = "buyDate")
+    public Date getBuyDate() {
+        return buyDate;
+    }
 
-	public void setStockName(String stockName) {
-		this.stockName = stockName;
-	}
+    public void setBuyDate(Date buyDate) {
+        this.buyDate = buyDate;
+    }
 
-	@Column(name = "buyDate")
-	public Date getBuyDate() {
-		return buyDate;
-	}
+    @Column(name = "recommedScore")
+    public Double getRecommedScore() {
+        return recommedScore;
+    }
 
-	public void setBuyDate(Date buyDate) {
-		this.buyDate = buyDate;
-	}
+    public void setRecommedScore(Double recommedScore) {
+        this.recommedScore = recommedScore;
+    }
 
-	@Column(name = "recommedScore")
-	public Double getRecommedScore() {
-		return recommedScore;
-	}
+    @Column(name = "buyCount")
+    public Integer getBuyCount() {
+        return buyCount;
+    }
 
-	public void setRecommedScore(Double recommedScore) {
-		this.recommedScore = recommedScore;
-	}
+    public void setBuyCount(Integer buyCount) {
+        this.buyCount = buyCount;
+    }
 
-	@Column(name = "buyCount")
-	public Integer getBuyCount() {
-		return buyCount;
-	}
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = result * 31 + (this.getBuyCode() == null ? 0 : this.getBuyCode().hashCode());
+        result = result * 31 + (this.getStockCode() == null ? 0 : this.getStockCode().hashCode());
+        result = result * 31 + (this.getBuyDate() == null ? 0 : this.getBuyDate().hashCode());
+        return result;
+    }
 
-	public void setBuyCount(Integer buyCount) {
-		this.buyCount = buyCount;
-	}
-
-	@Override
-	public int hashCode() {
-		int result = 17;
-		result = result * 31 + (this.getBuyCode() == null ? 0 : this.getBuyCode().hashCode());
-		result = result * 31 + (this.getStockCode() == null ? 0 : this.getStockCode().hashCode());
-		result = result * 31 + (this.getBuyDate() == null ? 0 : this.getBuyDate().hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (!(obj instanceof yybBuyStock))
-			return false;
-		yybBuyStock stock = (yybBuyStock) obj;
-		return stock.getBuyCode() == this.getBuyCode() && stock.getStockCode() == this.getStockCode() && stock.getBuyDate().equals(this.getBuyDate());
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof yybBuyStock))
+            return false;
+        yybBuyStock stock = (yybBuyStock) obj;
+        return stock.getBuyCode() == this.getBuyCode() && stock.getStockCode() == this.getStockCode() && stock.getBuyDate().equals(this.getBuyDate());
+    }
 }
