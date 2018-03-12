5917725584c6d91aaccf1d8f5a9c07806a157fd9
chpy <a910823>
2018-03-12 23:48:16 +0800

    股票走势统计

--- service/src/main/java/chen/spider/service/yybBuyStockService.java
+++ service/src/main/java/chen/spider/service/yybBuyStockService.java
@@ -14,6 +14,8 @@ public class yybBuyStockService {
     @Autowired
     yybBuyStockRepository yybBuyStockRepository;
 
+    private final String stockHistoryPriceUrl = "http://pdfm2.eastmoney.com/EM_UBG_PDTI_Fast/api/js?id=0023692&TYPE=k&style=top&num=10";
+
     public yybBuyStock save(yybBuyStock entity) {
         return yybBuyStockRepository.save(entity);
     }
@@ -21,5 +23,6 @@ public class yybBuyStockService {
     public List<yybBuyStock> save(List<yybBuyStock> entitys) {
         return ListUtil.toList(yybBuyStockRepository.save(entitys));
     }
+
 
 }
