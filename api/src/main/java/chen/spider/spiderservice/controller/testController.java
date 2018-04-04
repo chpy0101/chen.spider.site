4977daa3eaabfd525b7361663d5598ca6ef14d27
Administrator <540399769@qq.com>
2018-04-04 17:06:06 +0800

    位运算判断正负

--- api/src/main/java/chen/spider/spiderservice/controller/testController.java
+++ api/src/main/java/chen/spider/spiderservice/controller/testController.java
@@ -24,54 +24,54 @@ import java.util.stream.Stream;
 @RequestMapping("/test")
 public class testController {
 
-    @Autowired
-    yybBuyStockService yybBuyStockService;
+	@Autowired
+	yybBuyStockService yybBuyStockService;
 
-    @Autowired
-    private HttpServletRequest request;
+	@Autowired
+	private HttpServletRequest request;
 
-    @RequestMapping("/")
-    public String test() {
+	@RequestMapping("/")
+	public String test() {
 
-        eastmoneyChartsController spiderController = new eastmoneyChartsController(false);
-        spiderController.addFilter(new eastmoneyChartsFilter());
-        List<yybIncreaseEntity> yybEntity = spiderController.getData();
+		eastmoneyChartsController spiderController = new eastmoneyChartsController(false);
+		spiderController.addFilter(new eastmoneyChartsFilter());
+		List<yybIncreaseEntity> yybEntity = spiderController.getData();
 
-        List<yybBuyStock> stockInfos = yybEntity.stream().flatMap(t -> {
-            Stream<yybBuyStock> stock = t.getBuyStock().stream().map(s -> {
-                yybBuyStock temp = new yybBuyStock();
-                temp.setStockCode(s.getStockCode());
-                temp.setStockName(s.getStockName());
-                temp.setRecommedScore(0d);
-                temp.setBuyName("");
-                temp.setBuyDate(s.getBuyTime());
-                temp.setBuyCount(Double.valueOf(s.getBuyNum()).intValue());
-                temp.setBuyCode(s.getYybCode());
-                temp.setMaxHoldDay(t.getMaxRateDay());
-                return temp;
-            });
-            return stock;
-        }).distinct().collect(Collectors.toList());
-        if (stockInfos == null)
-            return "未获取到有效的股票信息";
-        List<yybBuyStock> result = yybBuyStockService.save(stockInfos);
-        return "OK";
-    }
+		List<yybBuyStock> stockInfos = yybEntity.stream().flatMap(t -> {
+			Stream<yybBuyStock> stock = t.getBuyStock().stream().map(s -> {
+				yybBuyStock temp = new yybBuyStock();
+				temp.setStockCode(s.getStockCode());
+				temp.setStockName(s.getStockName());
+				temp.setRecommedScore(0d);
+				temp.setBuyName("");
+				temp.setBuyDate(s.getBuyTime());
+				temp.setBuyCount(Double.valueOf(s.getBuyNum()).intValue());
+				temp.setBuyCode(s.getYybCode());
+				temp.setMaxHoldDay(t.getMaxRateDay());
+				return temp;
+			});
+			return stock;
+		}).distinct().collect(Collectors.toList());
+		if (stockInfos == null)
+			return "未获取到有效的股票信息";
+		List<yybBuyStock> result = yybBuyStockService.save(stockInfos);
+		return "OK";
+	}
 
-    @RequestMapping("/trend")
-    public String getStockTrend(@RequestParam(name = "date") Date date) {
-        //获取制定日期的推荐股票
-        List<yybBuyStock> stocks = yybBuyStockService.getRecommendStockByDay(date);
-        List<String> codes = stocks.stream().map(t -> t.getStockCode()).collect(Collectors.toList());
-        //获取相差天数
-        int days = DateUtil.getDifferTimes(new Date(), date, DifferTimeType.DAY_MILLISECOND);
-        eastmoneyStockController controller = new eastmoneyStockController(false, codes, days + 1);
-        List<stockPriceInfo> data = controller.getData();
-        Map<String, List<stockPriceInfo>> result = data.stream().collect(Collectors.groupingBy(stockPriceInfo::getStockCode));
-        result.forEach((key, value) -> value.sort((v1, v2) -> {
-            int differ = DateUtil.getDifferTimes(v1.getDate(), v2.getDate(), DifferTimeType.DAY_MILLISECOND);
-            return differ == 0 ? differ : (differ > 0 ? -1 : 1);
-        }));
-        return "";
-    }
+	@RequestMapping("/trend")
+	public String getStockTrend(@RequestParam(name = "date") Date date) {
+		//获取制定日期的推荐股票
+		List<yybBuyStock> stocks = yybBuyStockService.getRecommendStockByDay(date);
+		List<String> codes = stocks.stream().map(t -> t.getStockCode()).collect(Collectors.toList());
+		//获取相差天数
+		int days = DateUtil.getDifferTimes(new Date(), date, DifferTimeType.DAY_MILLISECOND);
+		eastmoneyStockController controller = new eastmoneyStockController(false, codes, days + 1);
+		List<stockPriceInfo> data = controller.getData();
+		Map<String, List<stockPriceInfo>> result = data.stream().collect(Collectors.groupingBy(stockPriceInfo::getStockCode));
+		result.forEach((key, value) -> value.sort((v1, v2) -> {
+			int differ = DateUtil.getDifferTimes(v1.getDate(), v2.getDate(), DifferTimeType.DAY_MILLISECOND);
+			return differ >> 31 | (~((~differ + 1) >> 31) + 1);
+		}));
+		return "";
+	}
 }
