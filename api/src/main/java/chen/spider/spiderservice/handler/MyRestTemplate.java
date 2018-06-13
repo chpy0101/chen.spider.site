f4486ecf433c9b3e37e8528fc783edfc5f2a1f4b
chpy <a910823>
2018-06-14 00:13:45 +0800

    统一objectMapper resttemplate日志监听

--- api/src/main/java/chen/spider/spiderservice/handler/MyRestTemplate.java
+++ api/src/main/java/chen/spider/spiderservice/handler/MyRestTemplate.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import java.lang.reflect.Field;
 import java.net.URI;
 import java.text.MessageFormat;
+import java.util.UUID;
 
 /**
  * Created by chpy on 18/6/6.
@@ -21,9 +22,13 @@ import java.text.MessageFormat;
 
 public class MyRestTemplate extends RestTemplate {
 
+    //错误次数过多时视为日志记录bug停止日志记录
+    private static Integer errorItmes = 0;
     private static Field requestField;
     Logger logger = LoggerFactory.getLogger(MyRestTemplate.class);
-    private final String requestInfo = "Request url:::::{0} ,method::::: {1}, data::::: {2}";
+    private final String requestInfo = "Request reqId:{0} ,url:{1} ,method:{2}, head:{3} ,data:{4}";
+    private final String responseInfo = "Response reqId:{0} ,body:{1}";
+    private final ObjectMapper objectMapper = (ObjectMapper) SpringUtils.getBean("myObjectMapper");
 
     /* 
      * 缓存获取的feild对象,减少反射
@@ -33,28 +38,30 @@ public class MyRestTemplate extends RestTemplate {
      * @return 
      */
     static {
-        Class requestCls = null;
-        Class<?>[] cls = RestTemplate.class.getDeclaredClasses();
-        for (Class cl : cls) {
-            if (cl.getName().equals("org.springframework.web.client.RestTemplate$HttpEntityRequestCallback")) {
-                requestCls = cl;
-                break;
-            }
-        }
         try {
-            requestField = requestCls.getDeclaredField("requestEntity");
-            requestField.setAccessible(true);
+            for (Class cl : RestTemplate.class.getDeclaredClasses())
+                if (cl.getName().equals("org.springframework.web.client.RestTemplate$HttpEntityRequestCallback")) {
+                    requestField = cl.getDeclaredField("requestEntity");
+                    requestField.setAccessible(true);
+                    break;
+                }
         } catch (NoSuchFieldException e) {
         }
     }
 
-    /* 
-     * 获取请求对象
-     * @author chenpengyue
-     * @date 18/6/10 上午12:24
-     * @param: [requestCallback]
-     * @return org.springframework.http.HttpEntity<?>
-     */
+
+    @Override
+    public <T> T doExecute(URI url, HttpMethod method, RequestCallback requestCallback, ResponseExtractor<T> responseExtractor) throws RestClientException {
+        String reqId = UUID.randomUUID().toString().replace("-", "");
+        //纪录请求体日志
+        recordRequest(reqId, url, method, requestCallback);
+        T response = super.doExecute(url, method, requestCallback, responseExtractor);
+        //纪录返回体日志
+        recordResponse(reqId, response);
+        return response;
+    }
+
+
     private HttpEntity getRequestEntity(RequestCallback requestCallback) throws IllegalAccessException {
         HttpEntity res = null;
         if (requestField != null)
@@ -62,25 +69,23 @@ public class MyRestTemplate extends RestTemplate {
         return res;
     }
 
-    @Override
-    public <T> T doExecute(URI url, HttpMethod method, RequestCallback requestCallback, ResponseExtractor<T> responseExtractor) throws RestClientException {
-
-        //纪录请求体日志
-        preExecute(url, method, requestCallback);
-        T response = super.doExecute(url, method, requestCallback, responseExtractor);
-        //纪录返回体日志
-        //logger.info();
-        return response;
+    private void recordRequest(String reqId, URI url, HttpMethod method, RequestCallback requestEntity) {
+        try {
+            HttpEntity resEntity = getRequestEntity(requestEntity);
+            String head = resEntity.getHeaders().getContentType().getType();
+            logger.info(MessageFormat.format(requestInfo, url.toString(), method.toString(), head, objectMapper.writeValueAsString(resEntity.getBody())));
+        } catch (Exception e) {
+            errorItmes++;
+            e.printStackTrace();
+        }
     }
 
-
-    public void preExecute(URI url, HttpMethod method, RequestCallback requestEntity) {
+    private <T> void recordResponse(String reqId, T response) {
         try {
-            ObjectMapper objectMapper = (ObjectMapper) SpringUtils.getBean("myObjectMapper");
-            HttpEntity resEntity = getRequestEntity(requestEntity);
-            logger.info(MessageFormat.format(requestInfo, url.toString(), method.toString(), objectMapper.writeValueAsString(resEntity.getBody())));
-        } catch (Exception e) {
-            e.printStackTrace();
+            logger.info(MessageFormat.format(responseInfo, reqId, objectMapper.writeValueAsString(response)));
+        } catch (Exception ex) {
+            errorItmes++;
+            ex.printStackTrace();
         }
     }
 
